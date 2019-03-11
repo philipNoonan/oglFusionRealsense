980df708e40485a47866cf3def3b8e958a3fe2b9
Phil Noonan <philip.noonan@outlook.com>
2019-03-11 08:36:34 +0000

    removed opencv dep

--- include/interface.h
+++ include/interface.h
@@ -21,10 +21,10 @@
 #include <mutex>
 #include <vector>
 
-#include "opencv2/core/utility.hpp"
-#include "opencv2/opencv.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/core/utility.hpp"
+//#include "opencv2/opencv.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/highgui/highgui.hpp"
 
 class Realsense2Camera
 {
